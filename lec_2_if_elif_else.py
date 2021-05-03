80c2228ae496abade448f634476c060aaf507455
Saga0001 <>
2021-05-03 13:35:56 +0000

    yo

---
+++ lec_2_if_elif_else.py
@@ -0,0 +1,14 @@
+a=3
+if a > 5:
+  print('hello5')
+elif a < 2:
+  print('hello 2')
+  
+if a > 5:
+  print('hello5')
+
+if a > 5:
+  print('hello5')
+else:
+    print('Tupo hello')  
+
---
+++ lec_2_logical operations.py
@@ -0,0 +1,10 @@
+a=3
+b=4
+c=5
+
+if a>4 and b==2:
+  print5 ('Good')
+elif b>5 or c==5:
+  print('Best') 
+else:
+  print('Bad')   
