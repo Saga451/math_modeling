3d502934c68b2b57d9732ca07552b4513fbd25d8
Saga0001 <>
2021-05-06 16:37:32 +0000

    +1

---
+++ lab_2_add_task_4.py
@@ -0,0 +1,9 @@
+a = int(input('Type the number which you wanna divide to simple numbers: '))
+
+while a != 1:
+    for b in range (2, a, 1):
+      d = a % b  
+      while d == 0:
+        print (f'{b}')
+        a = a / b
+        d = a % b
