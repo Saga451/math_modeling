9bf67af62eccebbb53b6148631d983e7595ae285
Saga0001 <82817268+Saga451@users.noreply.github.com>
2022-01-05 17:26:08 +0000

    4_add

--- lab_2_add_task_3.py
+++ lab_2_add_task_3.py
@@ -4,3 +4,18 @@ while a >= 0.1:
   b = a % 10
   print(b, end='')
   a = a // 10
+
+
+''' (тоже работает)
+a = int(input('type the volue: '))
+
+b = a // 10
+c = a % 10
+
+while b != 0:
+    print(f"{c}", end ='')
+    c = b % 10
+    b //= 10
+
+print(f'{c}')
+'''    
--- lab_2_add_task_4.py
+++ lab_2_add_task_4.py
@@ -1,7 +1,7 @@
 a = int(input('Type the number which you wanna divide to simple numbers: '))
 
 while a != 1:
-    for b in range (2, a, 1):
+    for b in range (2, a+1, 1):
       d = a % b  
       while d == 0:
         print (f'{b}')
