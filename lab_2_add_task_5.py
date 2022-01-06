1fa8099e3d74c05c6ad24c771eb9096b02185b1d
Saga0001 <82817268+Saga451@users.noreply.github.com>
2022-01-06 15:01:20 +0000

    add_5

---
+++ lab_2_add_task_5.py
@@ -0,0 +1,16 @@
+a = int(input()) #совершенные числа
+
+for b in range (1, a+1, 1):
+  e = 0
+  for c in range(1, b+1, 1):
+        
+        if b % c == 0:
+            e += c
+            #print(f'{b} - {c}')
+        else:
+            continue
+  #print(e)
+  if b == e / 2:
+      print(b)
+  else: 
+      continue  
