09df8ae6760e66629319aef087f0f62d7e884ed9
Saga0001 <82817268+Saga451@users.noreply.github.com>
2022-01-06 15:04:39 +0000

    5

--- lab_2_add_task_5.py
+++ lab_2_add_task_5.py
@@ -14,3 +14,19 @@ for b in range (1, a+1, 1):
       print(b)
   else: 
       continue  
+
+
+''' 
+без лишнего кода
+
+a = int(input())
+
+for b in range (1, a+1, 1):
+  e = 0
+  for c in range(1, b+1, 1):
+        if b % c == 0:
+            e += c
+  if b == e / 2:
+      print(b)
+
+'''  
