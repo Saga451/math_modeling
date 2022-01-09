fef515e6f2797bdcd770f100c5a9e757063cb3d2
Saga0001 <82817268+Saga451@users.noreply.github.com>
2022-01-09 22:57:03 +0000

    .

--- lab_3/lab_3_add_task_2.py
+++ lab_3/lab_3_add_task_2.py
@@ -4,20 +4,15 @@ a = np.ndarray((5))
 
 for d in range(0, 4, 1):
   a[d] = int(input('Type the volue: '))
-  
-
-print(a)
 
 c = int(input("Pepe i: "))
-a[c] = int(input('Type!!: '))
 
-print(a)
-
-
-for i in range(0, 5, 1):
+for i in range(4, -1, -1):
+  d = i
   
   if c < i:
     a[i] = a [i-1]
 
+a[c] = int(input('Type!!: '))
 
 print(a)
