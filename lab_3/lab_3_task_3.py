dbb63b78e64854592c6cfd25be91537f221a0835
sagasgit <82817268+sagasgit@users.noreply.github.com>
2023-02-06 15:42:06 +0200

    Update lab_3_task_3.py

--- lab_3/lab_3_task_3.py
+++ lab_3/lab_3_task_3.py
@@ -1,15 +1,22 @@
 import numpy as np 
 from lab_3_task_1 import g
 
-x0 = 1
-y0 = 1 
-v0 = 1 
+v0 = int(input("Type the start velocity: "))
+a = float(input("Type the angle of the throw: pi * "))
 
-for t in np.linspace(0, 5, 11):
+vx0 = np.cos(np.pi * a) * v0 
+vy0 = np.sin(np.pi * a) * v0
 
-  x = x0 + v0 * t 
-  y = y0 + v0 * t - (g * (t**2)) / 2
+for t in np.arange(0, 100, 0.2):
 
-  tb = [t, x, y]
-  print(tb)
- 
+    x = vx0 * t
+    y = vy0 * t - (g * (t ** 2)) / 2
+    
+    x1 = x // 1
+    y1 = y // 1
+    
+    if y <= 0  and t != 0:
+        break
+
+    tb = [t, x1, y1]
+    print(tb)
